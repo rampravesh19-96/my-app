8237db259151dea7abbc5fa2e3a34b8dba55321b
ramps <rampraveshsingh1996@gmail.com>
2023-05-08 19:31:47 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
-import Home from './components/Home';
-import Facebook from './components/Facebook';
 
 function App(props) {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/facebook' element={<Facebook/>}/>
-    </Routes>
+    <div>
+      hello worldddddddddddd
+    </div>
   );
 }
 
