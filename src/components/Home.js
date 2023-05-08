0d7dc0ec27dceecff767bfa3f184db7bbe79e611
ramps <rampraveshsingh1996@gmail.com>
2023-05-08 19:59:07 +0530

    xxx

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home(props) {
     return (
         <div>
-            Home
+            <Link to='/facebook'>facebook</Link>
         </div>
     );
 }
